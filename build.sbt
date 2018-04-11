0df750d253578b68781de2559490a14854d12e7b
Stanislav Poslavsky <stvlpos@mail.ru>
2018-04-11 21:47:06 +0300

    Update build.sbt to 1.1

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ organization := "cc.redberry"
 
 name := "algebench"
 
-version := "1.0"
+version := "1.1"
 
 scalaVersion := "2.12.3"
 
--- src/test/scala/cc/redberry/algebench/GroebnerBasisDataTest.scala
+++ src/test/scala/cc/redberry/algebench/GroebnerBasisDataTest.scala
@@ -1,29 +1,27 @@
 package cc.redberry.algebench
 
-import java.io.{FileOutputStream, ObjectOutputStream}
-import java.util.zip.GZIPOutputStream
-
-import org.junit.Test
+import org.junit.{Ignore, Test}
 
 /**
   *
   */
 class GroebnerBasisDataTest {
+  @Ignore
   @Test
   def test1(): Unit = {
     println(GroebnerBasisData.retrieveAllSystems().map(_.name).sorted.mkString("\n"))
   }
 
-//  @Test
-//  def test2(): Unit = {
-//    val datas: Array[GroebnerBasisData] = GroebnerBasisData.retrieveAllSystems().toArray
-//    val sss = new ObjectOutputStream(new GZIPOutputStream(new FileOutputStream("/Users/poslavskysv/Projects/redberry2/algebench/src/main/resources/gb.data.gz")))
-//    sss.writeObject(datas)
-//    sss.close()
-//  }
-//
-//  @Test
-//  def test3(): Unit = {
-//    println(GroebnerBasisData.allSystems())
-//  }
+  //  @Test
+  //  def test2(): Unit = {
+  //    val datas: Array[GroebnerBasisData] = GroebnerBasisData.retrieveAllSystems().toArray
+  //    val sss = new ObjectOutputStream(new GZIPOutputStream(new FileOutputStream("src/main/resources/gb.data.gz")))
+  //    sss.writeObject(datas)
+  //    sss.close()
+  //  }
+  //
+  //  @Test
+  //  def test3(): Unit = {
+  //    println(GroebnerBasisData.allSystems())
+  //  }
 }
